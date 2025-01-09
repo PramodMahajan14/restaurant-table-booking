e8577be99bf588dabcfcb5d486198adbbc711e72
Pramod <pramodmahajan1411200@gmail.com>
2025-01-09 15:22:09 +0530

    Updated auth layout 1

--- components/time-picker.tsx
+++ components/time-picker.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import * as React from "react";
-import { format } from "date-fns";
-import { ChevronDown, Clock } from "lucide-react";
+
+import { Clock } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
+
 import {
   Popover,
   PopoverContent,
@@ -14,36 +14,20 @@ import {
 } from "@/components/ui/popover";
 
 export const DatePicker = () => {
-  const [date, setDate] = React.useState<Date>();
-
   return (
     <Popover>
       <PopoverTrigger asChild>
         <Button
           variant={"outline"}
           className={cn(
-            "max-w-[250px]  justify-start text-left font-normal text-xs md:text-sm",
-            !date && "text-muted-foreground"
+            "max-w-[250px]  justify-start text-left font-normal text-xs md:text-sm"
           )}
         >
           <Clock />
-          {date ? (
-            format(date, "PPP")
-          ) : (
-            <span className="flex items-center md:space-x-4">
-              {" "}
-              <p>Date</p> <ChevronDown className="w-4 h-4" />{" "}
-            </span>
-          )}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0" align="start">
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={setDate}
-          initialFocus
-        />
+        comming soon
       </PopoverContent>
     </Popover>
   );
